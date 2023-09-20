feeda8c46abd32a32ecf207fdc2546d8321ad095
kevsalas <kevinsalas50@hotmail.com>
2023-09-19 23:01:59 -0400

    Changed C# Prep 1 and 2 to do the work

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentaje? ");
+        string grade = Console.ReadLine();
+        int userGrade = int.Parse(grade);
+
+        // Giving grades in order
+        int gradeA = 90;
+        string gradeLetterA = "A";
+        //////////////////////////
+        int gradeB = 80;
+        string gradeLetterB = "B";
+        //////////////////////////
+        int gradeC = 70;
+        string gradeLetterC = "C";
+        /////////////////////////
+        int gradeD = 60;
+        string gradeLetterD = "D";
+        // Grade F IS IT less than 60
+        string gradeLetterF = "F";
+        if (userGrade >= gradeA)
+        {
+            Console.WriteLine($"Congrats your grade is {gradeLetterA}!");
+        }
+        else if (userGrade >= gradeB && userGrade < gradeA)
+        {
+            Console.WriteLine($"Congrats your grade is {gradeLetterB}");
+        }
+           else if (userGrade >= gradeC && userGrade < gradeB)
+        {
+            Console.WriteLine($"Congrats your grade is {gradeLetterC}");
+        }
+           else if (userGrade >= gradeD && userGrade < gradeC)
+        {
+            Console.WriteLine($"Congrats your grade is {gradeLetterD}");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {gradeLetterF}");
+        }
+
+        
     }
 }
